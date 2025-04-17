5263dc71ea3c2b83d81f3cad3a64051a03d19206
Ross Grady <ross@rossgrady.org>
2025-04-16 21:19:09 -0400

    finished tribe & added motorco

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from parsers import rhp_parser, tribe_parser
+from parsers import rhp_parser, tribe_parser, tickera_parser, ical_parser
 from operator import itemgetter
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 env = Environment(
@@ -7,18 +7,12 @@ env = Environment(
 )
 template = env.get_template("roksme_index.html")
 
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'}
-
-#venues_list = [{'name':"Cat's Cradle", 'url':'https://catscradle.com/events/?view=list', 'parser':rhp_parser},
-# {'name': "Local 506", 'url':'https://local506.com/events/?view=list', 'parser':rhp_parser},
- # {'name':"The Pinhook", 'url':'https://thepinhook.com/events/?view=list', 'parser':rhp_parser}]
-
-venues_list = [{'name':"The Cave", 'url':'https://caverntavern.com/calendar/', 'parser':tribe_parser}]
+venues_list = [{'name':"Cat's Cradle", 'url':'https://catscradle.com/events/?view=list', 'parser':rhp_parser}, {'name': "Local 506", 'url':'https://local506.com/events/?view=list', 'parser':rhp_parser}, {'name':"The Pinhook", 'url':'https://thepinhook.com/events/?view=list', 'parser':rhp_parser}, {'name':"Slim's", 'url':'https://slimsraleigh.com/calendar/list/', 'parser': tribe_parser},{'name':"The Cave", 'url':'https://caverntavern.com/calendar/list/', 'parser': tribe_parser}, {'name':"Motorco", 'url':'https://motorcomusic.com/', 'parser': tickera_parser}]
 
 def main():
   events_array = []
   for venue in venues_list:
-    venue_array = venue['parser'](venue['name'], venue['url'], headers)
+    venue_array = venue['parser'](venue['name'], venue['url'])
     events_array = events_array + venue_array
   events_sorted = sorted(events_array, key=itemgetter('event_date'))
   template.stream(events=events_sorted).dump('index.html')
--- parsers.py
+++ parsers.py
@@ -2,10 +2,26 @@ from bs4 import BeautifulSoup
 import html5lib
 import requests
 import dateparser
+import icalendar
 
-def rhp_parser(venue_name, url, headers):
+stopwords = ["Karaoke", "KARAOKE", "Dance Party", "DANCE PARTY", "CALENDAR", "Wild Turkey Thursday", "Private Event Upstairs", "Open Mic", "Music Trivia", "Tequila Tuesday", "Pangean", "CANCELED", "VERANDA PARTY", "DRAG BINGO"]
+
+def retrieve(url):
+  headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'}
   response = requests.get(url, headers=headers)
-  html_content = response.text
+  return response.text
+
+def retrieve_tickera(url):
+  headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'}
+  session = requests.Session()
+  response = session.get(url, headers=headers)
+  payload = {'action': 'tc_filter_events', 'tc_categories': '0', 'tc_start_date': '0', 'tc_column_number': '1', 'tc_show_excerpt': 'true', 'tc_show_number_of_posts': '200', 'tc_pagination_number': '1', 'tc_show_default_featured_image': 'true', 'tc_show_past_events': 'false'}
+  real_url = url + "/wp-admin/admin-ajax.php"
+  real_response = session.post(real_url, headers=headers, data=payload)
+  return real_response.text
+
+def rhp_parser(venue_name, url):
+  html_content = retrieve(url)
   soup = BeautifulSoup(html_content, 'html5lib')
   events = soup(class_="rhpSingleEvent")
   event_array = []
@@ -32,41 +48,98 @@ def rhp_parser(venue_name, url, headers):
     event_dict['event_date'] = show_date
     event_dict['human_date'] = show_date.strftime('%a %d %b %Y')
     event_dict['more_url'] = more_url
-    event_array.append(event_dict)
+    flag = False
+    for word in stopwords:
+      if word in event_string:
+        flag = True
+    if flag == False:
+      event_array.append(event_dict)
   return event_array
 
+def tribe_parser(venue_name, url):
+  event_array = []
+  for i in range(1,7):
+    if i < 2:
+      paginated_url = url
+    else:
+      paginated_url = url + "page/" + str(i) + "/"
+    html_content = retrieve(paginated_url)
+    soup = BeautifulSoup(html_content, 'html5lib')
+    if soup(class_="tribe-events-c-messages__message-list-item"):
+      messages = soup(class_="tribe-events-c-messages__message-list-item")
+      if messages[0].string.strip() == "There were no results found.":
+        return event_array
+    events = soup(class_="tribe-events-calendar-list__event-row")
+    for event in events:
+      event_dict = {}
+      more_url = event.find(class_="tribe-events-calendar-list__event-title").a['href']
+      event_string = event.find(class_="tribe-events-calendar-list__event-title-link").string.strip()
+      venue_url = url
+      openers_array = event.find_all(class_="bands-row")
+      if(openers_array):
+        if len(openers_array) > 0:
+          for opener in openers_array:
+            opener_name = opener.string.strip()
+            if opener_name not in event_string:
+              event_string = event_string+" / "+opener_name
+      date = event.find(class_="tribe-events-calendar-list__event-date-tag-datetime")['datetime']
+      show_date = dateparser.parse(date)
+      event_dict['venue_name'] = venue_name
+      event_dict['venue_url'] = venue_url
+      event_dict['event_string'] = event_string
+      event_dict['event_date'] = show_date
+      event_dict['human_date'] = show_date.strftime('%a %d %b %Y')
+      event_dict['more_url'] = more_url
+      flag = False
+      for word in stopwords:
+        if word in event_string:
+          flag = True
+      if flag == False:
+        event_array.append(event_dict)
+  return event_array
 
-def tribe_parser(venue_name, url, headers):
-  response = requests.get(url, headers=headers)
-  html_content = response.text
+def ical_parser(venue_name, url):
+  ical_content = retrieve(url)
+  # now we need to parse that ical!
+  calendar = icalendar.Calendar.from_ical(ical_content)
+  for event in calendar.events:
+    print(event.get("SUMMARY"))
+    print(event.get("DTSTART"))
+    print(event.get("DESCRIPTION"))
+    print("---")
+
+def tickera_parser(venue_name, url):
+  html_content = retrieve_tickera(url)
   soup = BeautifulSoup(html_content, 'html5lib')
-  events = soup(class_="tribe-events-calendar-list__event-row")
+  events = soup(class_="tc-single-event")
   event_array = []
   for event in events:
     event_dict = {}
-    more_url = event.find(class_="tribe-events-calendar-list__event-title").a['href']
-    event_string = event.find(class_="tribe-events-calendar-list__event-title-link").string.strip()
+    title = event.find('h4').a.string.strip()
+    event_string = title
     venue_url = url
-    openers_array = event.find_all(class_="bands-row")
-    if(openers_array):
-      print(openers_array)
-      if len(openers_array) > 0:
-        for opener in openers_array:
-          print(opener)
-          opener_name = opener.string.strip()
-          event_string = event_string+", "+opener_name
-    #venue = event.find(class_="venueLink")
-    #if venue:
-    #  venue_url = venue['href']
-    #  venue_name = venue.string.strip()
-    date = event.find(class_="tribe-events-calendar-list__event-date-tag-datetime")['datetime']
+    opener = event.find(class_="with")
+    if(opener):
+      opener_name = opener.next_sibling.strip()
+      if len(opener_name) > 0:
+        event_string = title+" / "+opener_name
+    venue = event.find(class_="venueLink")
+    if venue:
+      venue_url = venue['href']
+      venue_name = venue.string.strip()
+    date = event.find(class_="tc-event-date").span.string.strip()
     show_date = dateparser.parse(date)
-    
+    more_url = event.find('h4').a['href']
     event_dict['venue_name'] = venue_name
     event_dict['venue_url'] = venue_url
     event_dict['event_string'] = event_string
     event_dict['event_date'] = show_date
     event_dict['human_date'] = show_date.strftime('%a %d %b %Y')
     event_dict['more_url'] = more_url
-    event_array.append(event_dict)
+    flag = False
+    for word in stopwords:
+      if word in event_string:
+        flag = True
+    if flag == False:
+      event_array.append(event_dict)
   return event_array
